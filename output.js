10a8b276fffe45190f1c064e8c762b3468e02b9f
action <action@github.com>
2025-01-03 10:29:42 +0000

    Add decoded output file

--- output.js
+++ output.js
@@ -1,17 +1,20 @@
-//Mon Dec 30 2024 15:20:33 GMT+0000 (Coordinated Universal Time)
+//Fri Jan 03 2025 10:29:42 GMT+0000 (Coordinated Universal Time)
 //Base:https://github.com/echo094/decode-js
 //Modify:https://github.com/smallfawn/decode_action
-function imgsrc(_0x56bf0b, _0x2cc5b2, _0x273bc7) {
-  _0x2cc5b2 = "453$asd#4";
-  const _0x237a71 = CryptoJS.enc.Utf8.parse(_0x2cc5b2.substring(0, 16));
-  _0x2cc5b2 = CryptoJS.enc.Utf8.parse(CryptoJS.MD5(_0x2cc5b2).toString());
-  if (_0x273bc7) return CryptoJS.RC4.decrypt(_0x56bf0b, _0x2cc5b2, {
-    "iv": _0x237a71,
-    "padding": CryptoJS.pad.Pkcs7
-  }).toString(CryptoJS.enc.Utf8);
-  return CryptoJS.RC4.encrypt(_0x56bf0b, _0x2cc5b2, {
-    "iv": _0x237a71,
-    "mode": CryptoJS.mode.CBC,
-    "padding": CryptoJS.pad.Pkcs7
-  }).toString();
-}
+(() => {
+  const _0x2c3a05 = "https://ipv4.ip.mir6.com/api/api_json.php?ip=myip&token=%5Btoken+redacted%5D";
+  async function _0x255892() {
+    const _0xc4a312 = document.getElementById("output-ipv4");
+    try {
+      const _0x4f6f3d = await fetch(_0x2c3a05),
+        _0x3ac798 = await _0x4f6f3d.json();
+      if (_0x3ac798.success) {
+        const _0x3d7f83 = "\n- IP 鍦板潃锛�" + _0x3ac798.data.ip + "\n- 国家：" + _0x3ac798.data.country + " (" + _0x3ac798.data.countryCode + ")\n- 省份：" + _0x3ac798.data.province + "\n- ISP：" + _0x3ac798.data.isp + "\n- 网络类型：" + _0x3ac798.data.net + "\n                ";
+        _0xc4a312.innerHTML = "<pre>" + _0x3d7f83 + "</pre>";
+      } else _0xc4a312.innerHTML = "<strong>查询失败：</strong> " + _0x3ac798.message;
+    } catch (_0x222fb4) {
+      _0xc4a312.innerHTML = "<strong>错误：</strong> 无法获取数据，请稍后重试。";
+    }
+  }
+  _0x255892();
+})();
